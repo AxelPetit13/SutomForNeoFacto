0bd1638667634513cce44e00d2332c1535428575
PETIT Axel <axel.petit@enseirb.fr>
2024-02-12 12:11:08 +0100

    The color of the keyboard keys changes

--- frontend/src/app/components/keyboard/keyboard.component.ts
+++ frontend/src/app/components/keyboard/keyboard.component.ts
@@ -1,21 +1,27 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GameService } from '../../services/game.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'keyboard',
   templateUrl: './keyboard.component.html',
   styleUrl: './keyboard.component.css',
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, NgClass],
 })
 export class KeyboardComponent {
   protected word = '';
+  protected colors: { [key: string]: string } = {};
   @ViewChild('input') input: ElementRef<HTMLInputElement> | undefined;
 
   constructor(protected gameService: GameService) {
     this.word = gameService.firstLetter;
     this.gameService = gameService;
+    for (let i = 97; i <= 122; i++) {
+      const letter = String.fromCharCode(i); // Convertir le code ASCII en lettre
+      this.colors[letter] = 'black'; // Définir la valeur de la lettre sur 'black'
+    }
   }
 
   // Focus the input element just after the view is initialized
@@ -36,6 +42,7 @@ export class KeyboardComponent {
 
     // If no problem, clear the input
     if (gameAnswer !== null) {
+      this.findCorrectLetters();
       if (this.input !== undefined) {
         this.input.nativeElement.value = this.gameService.firstLetter;
       }
@@ -58,6 +65,24 @@ export class KeyboardComponent {
         -1,
       );
       this.gameService.currentAttempt = this.input.nativeElement.value;
+    }
+  }
+
+  private findCorrectLetters() {
+    const attempts = this.gameService.attempts;
+    const attemptsAnswers = this.gameService.attemptsAnswers;
+
+    const letters = [];
+    for (let i = 0; i < attempts.length; i++) {
+      for (let j = 0; j < attempts[i].length; j++) {
+        if (attemptsAnswers[i][j] !== 'INCORRECT') {
+          this.colors[`${attempts[i][j]}`] = '*';
+        } else {
+          if (this.colors[`${attempts[i][j]}`] !== '*') {
+            this.colors[`${attempts[i][j]}`] = 'grey';
+          }
+        }
+      }
     }
   }
 }
